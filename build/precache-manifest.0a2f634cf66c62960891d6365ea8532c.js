5adf9688384bd114db80bdc57192fff37ffdc145
Troublesome Saramsh <54758588+TroublesomeSaramsh789@users.noreply.github.com>
2020-10-05 09:47:00 +0545

    Add files via upload
    Final Version v1.0
    Visit Bishnu Electricals Online Store.
    Link: https://bishnu-electricals-store.web.app/

---
+++ build/precache-manifest.0a2f634cf66c62960891d6365ea8532c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d0d9f965f8b6610ee7c74795901b9e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76e9df6882207a1e52e1",
+    "url": "/static/css/main.a057f536.chunk.css"
+  },
+  {
+    "revision": "2493ab50b4298575fc13",
+    "url": "/static/js/2.402746cf.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/static/js/2.402746cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76e9df6882207a1e52e1",
+    "url": "/static/js/main.40b7f172.chunk.js"
+  },
+  {
+    "revision": "5387050906b8ed53cb42",
+    "url": "/static/js/runtime-main.ec2d9410.js"
+  },
+  {
+    "revision": "3f1c893b4963e7857b287c76eded8ac2",
+    "url": "/static/media/Bishnu.3f1c893b.png"
+  }
+]);
